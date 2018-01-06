448e38edeb623cef63bacda2e4a192a7f65e3528
spi <pspiess@protonmail.com>
2018-01-06 23:46:34 +0100

    Implement guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,22 @@
 # guessinggame
+
+function compare {
+  if [[ $1 -gt $2 ]]
+  then
+    echo 'Too high - try again with a lower number!'
+  else
+    echo 'Too low - try again with a higher number!'
+  fi
+}
+
+filesInDir=$(ls -l | wc -l)
+while 
+    echo 'Guess the number of files in the current directory:'
+    read  inputNumber
+    compare $inputNumber $filesInDir
+    [[ $inputNumber -ne  $filesInDir ]]
+    do :; 
+done
+
+echo 'You guessed the correct number of files in directory:'
+echo $(pwd)
